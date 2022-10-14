5f8a1939466bea5912db69c129bc15e735982331
josue023 <josuejun7@gmail.com>
2022-10-14 10:43:19 -0500

    Tarea 2 javascript

---
+++ ejercicio_2/index.js
@@ -0,0 +1,14 @@
+let nombre = "Josue";
+let edad = 26;
+let EresDesarrollador = true
+let fechaNacimiento = new Date(1996, 4, 8)
+let libroFav = {
+    titulo: "Code complete",
+    autor: "Steve McConnell",
+    fecha: new Date(2004, 5, 19),
+    url: "https://www.amazon.com/-/es/Steve-McConnell/dp/0735619670/ref=sr_1_1?adgrpid=81816131496&gclid=EAIaIQobChMIv7qy-ITg-gIVrsyGCh2ixAqYEAAYASAAEgIPwvD_BwE&hvadid=585362809884&hvdev=c&hvlocphy=1005376&hvnetw=g&hvqmt=e&hvrand=12479283340732746641&hvtargid=kwd-27623266&hydadcr=22335_13333110&keywords=code+complete&qid=1665761445&qu=eyJxc2MiOiIxLjQyIiwicXNhIjoiMS4xMSIsInFzcCI6IjEuMjIifQ%3D%3D&sr=8-1"
+}
+
+const datos = [nombre, edad, EresDesarrollador, fechaNacimiento, libroFav]
+
+console.log(datos)
