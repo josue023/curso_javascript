19c6090737e84c7d4468b90d07228375f89073e1
josue023 <josuejun7@gmail.com>
2022-11-29 10:09:58 -0500

    tarea 5 de javascript

---
+++ ejercicio_5/ejercicio5.js
@@ -0,0 +1,19 @@
+let altura = 170;
+let altura2 = altura / 100;
+let peso = 70;
+
+let altura3 = Math.ceil(altura2);
+console.log(altura3);
+
+let peso2 = Math.floor(peso);
+console.log(peso2)
+
+let esIgual = false;
+let max_valor_js = Number.MAX_VALUE;
+let min_valor_js = Number.MIN_VALUE;
+
+if (max_valor_js + 1 == max_valor_js) {
+    esIgual = true;
+}
+
+console.log(esIgual)
