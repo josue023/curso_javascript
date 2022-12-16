b9bf61674c491c78c221834bbe3d4b1c7cf8d368
josue023 <josuejun7@gmail.com>
2022-12-16 11:22:20 -0500

    tarea 13 javascript eslint

---
+++ ejercicio_13/eslint-npm/cadenas_texto.js
@@ -0,0 +1,18 @@
+let nombre = "Josue"
+let apellido = 'junco'
+let estudiante = nombre.concat(" ", apellido);
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+let numLetras = estudiante.length;
+console.log(numLetras);
+
+  let primerLetra = estudiante.charAt();
+let ultimaLetra = estudiante.charAt(estudiante.length - 1);
+console.log(ultimaLetra)
+
+console.log(estudiante.replace(" ", ""));
+
+let comprobarNombre = estudiante.includes("Josue")
+console.log(comprobarNombre)
