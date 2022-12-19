277b386c83c354260f2994d19c9aa672591472ba
josue023 <josuejun7@gmail.com>
2022-12-19 12:18:26 -0500

    tarea 14 eventos y jquery

---
+++ ejercicio_14/animacion.js
@@ -0,0 +1,5 @@
+$(() => {
+    $("#btn-1").click(() => {
+        console.log("Hola, estoy utilizando jQuery")
+    })
+})
---
+++ ejercicio_14/index.js
@@ -0,0 +1,5 @@
+const boton = document.getElementById("btn-1")
+
+boton.addEventListener("click", ()=>{
+    alert("Click en el botón")
+})
